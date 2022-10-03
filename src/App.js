106a50da3447180d9a618fd68a897d618b314412
Ayush Pandey <60148537+ayushpandey830@users.noreply.github.com>
2022-10-03 18:33:53 +0530

    Updated App.js for feature enhancement
    I have done changes in order to warn the user for any clash in event dates i.e. If the newly added event is clashing with previous event then there will be a ALERT message before adding event.

--- src/App.js
+++ src/App.js
@@ -44,6 +44,32 @@ function App() {
     const [allEvents, setAllEvents] = useState(events);
 
     function handleAddEvent() {
+        
+        for (let i=0; i<allEvents.length; i++){
+
+            const d1 = new Date (allEvents[i].start);
+            const d2 = new Date(newEvent.start);
+            const d3 = new Date(allEvents[i].end);
+            const d4 = new Date(newEvent.end);
+      /*
+          console.log(d1 <= d2);
+          console.log(d2 <= d3);
+          console.log(d1 <= d4);
+          console.log(d4 <= d3);
+            */
+
+             if (
+              ( (d1  <= d2) && (d2 <= d3) ) || ( (d1  <= d4) &&
+                (d4 <= d3) )
+              )
+            {   
+                alert("CLASH"); 
+                break;
+             }
+    
+        }
+        
+        
         setAllEvents([...allEvents, newEvent]);
     }
 
